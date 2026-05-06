1e6e6c296e3e33952f3ddd47e4a8a2213ee55706
Fakar uddin <fakaruddin@Fakar-ka-laipatopa.local>
2026-05-06 20:12:43 +0530

    first commit

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ import certifi
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://fakar2419_db_user:xlQ1HCtfk5diaNpp@cluster0.gbb4cqq.mongodb.net/?appName=Cluster0")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://fakar2419_db_user:SfBeVFB6r4ERFphF@cluster0.gbb4cqq.mongodb.net/?appName=Cluster0")
 client = AsyncIOMotorClient(MONGODB_URI, uuidRepresentation="standard", tlsCAFile=certifi.where(), tlsAllowInvalidCertificates=True)
 
 # Default database for PBAS
